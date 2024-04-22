88d805640eded41a40c478ceec080129667ff287
Yu Sun <yu.sun.cs@gmail.com>
2024-04-22 16:55:03 -0700

    update

--- js/res.js
+++ js/res.js
@@ -9,8 +9,8 @@ function showRes() {
 		var isFound = false;
 		for(var i = 0; i < stuDb.length; i++) {
 			// console.log(stuDb[i]);
-			if (stuDb[i]['LastName'].toUpperCase() == lastName.toUpperCase() && 
-				stuDb[i]['Email'].toUpperCase() == email.toUpperCase()) {				
+			if (stuDb[i]['lastname'].toUpperCase() == lastName.toUpperCase() && 
+				stuDb[i]['email'].toUpperCase() == email.toUpperCase()) {				
 
 				isFound = true;
 				var student = stuDb[i];
@@ -19,15 +19,15 @@ function showRes() {
 			"<h3>Congratulations!</h3>";
 		
 				document.getElementById("result_table").innerHTML += 
-					'<tr><td>Name</td><td>' + student.FirstName + ' ' + student.LastName + '</td></tr>' + 
-					'<tr><td>Grade</td><td>' + student.GradeLevel + '</td></tr>' + 					
-					'<tr><td>Project Title</td><td>' + student.Title + '</td></tr>' + 
-					'<tr><td>Video</td><td><a href="'+ student.Video + '">' + student.Video + '</a></td></tr>' + 
-					'<tr><td>Division</td><td>' + student.Division + '</td></tr>' + 
-					'<tr><td>Score</td><td>' + student.Score + '/100</td></tr>' + 
-					'<tr><td>Rank</td><td>' + student.Rank + '</td></tr>' + 
-					'<tr><td>Award</td><td><b style="color: red;">' + student.Award + '</b></td></tr>' + 
-					'<tr><td>Judge Comment</td><td>' + student.Comment + '</td></tr><br>';
+					'<tr><td>Name</td><td>' + student.firstname + ' ' + student.lastname + '</td></tr>' + 
+					'<tr><td>Grade</td><td>' + student.grade + '</td></tr>' + 					
+					'<tr><td>Project Title</td><td>' + student.title + '</td></tr>' + 
+					'<tr><td>Video</td><td><a href="'+ student.youtube + '" target="_blank">' + student.youtube + '</a></td></tr>' + 
+					'<tr><td>Division</td><td>' + student.division + '</td></tr>' + 
+					'<tr><td>Score</td><td>' + student.score + '/100</td></tr>' + 
+					'<tr><td>Rank</td><td>' + student.rank + '</td></tr>' + 
+					'<tr><td>Award</td><td><b style="color: red;">' + student.award + '</b></td></tr>' + 
+					'<tr><td>Judge Comment</td><td>' + student.comment + '</td></tr><br>';
 			}
 		}
 		if (!isFound) {
@@ -35,7 +35,7 @@ function showRes() {
 			"<h3>Incorrect email or last name. <br> Please contact <a>contact@gamegala.org</a></h3>";
 		} else {
 			document.getElementById("msg").innerHTML =
-			"<h3>If you find any incorrect information or have questions about the result, please contact <a>contact@gamegala.org</a></h3>";
+			"<h3>Your certificate will be emailed separately. If you find any incorrect information or have questions about the result, please contact <a>contact@gamegala.org</a></h3>";
 		}
 	} 
 }
